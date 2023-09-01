a118284ea1c2db1935050bcf4745cf2d994a329d
Ximena Elgart <ximenaelgart.dev@gmail.com>
2023-09-01 12:38:28 -0300

    fix due list style

--- src/app/modules/presentation/views/home/home-project-list/home-project-list.component.ts
+++ src/app/modules/presentation/views/home/home-project-list/home-project-list.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService } from 'src/app/modules/core/services/project/project.service';
 import { Project } from 'src/app/modules/models/project.model';
-import { puffAnimation } from 'src/assets/styles/animations';
 
 @Component({
   selector: 'app-home-project-list',
   templateUrl: './home-project-list.component.html',
   styleUrls: ['./home-project-list.component.scss'],
-  animations:[puffAnimation],
 })
 export class HomeProjectListComponent implements OnInit {
   projects: Project[] = [];
